60708e5084b1e1b32a57edde0a656e7a261f6d8a
mohamedlotfy <hammad.mohamed1893@gmail.com>
2024-06-30 21:16:48 +0200

    update the service added the fucking

--- src/main/java/com/momo/ws/api/keycloak/examplesimpleservice/GreetingController.java
+++ src/main/java/com/momo/ws/api/keycloak/examplesimpleservice/GreetingController.java
@@ -10,10 +10,16 @@ import org.springframework.web.bind.annotation.RestController;
 public class GreetingController {
 
     private static final String template = "Hello, %s!";
+    private static final String template_f = "fuck you, %s!";
     private final AtomicLong counter = new AtomicLong();
 
     @GetMapping("/greeting")
     public Greeting greeting(@RequestParam(value = "name", defaultValue = "All") String name) {
         return new Greeting(counter.incrementAndGet(), String.format(template, name));
+    }
+
+    @GetMapping("/fuckyou")
+    public Greeting fucking(@RequestParam(value = "name", defaultValue = "All") String name) {
+        return new Greeting(counter.incrementAndGet(), String.format(template_f, name));
     }
 }
