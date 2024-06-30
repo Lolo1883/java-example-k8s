3a9defe8b10c403b26a32add0506da17f5ad4ea5
mohamedlotfy <hammad.mohamed1893@gmail.com>
2024-06-30 20:40:25 +0200

    extracting all files out so the ./gradlew is in root

--- ExampleSimpleService/src/main/java/com/momo/ws/api/keycloak/examplesimpleservice/Greeting.java
+++
@@ -1,4 +0,0 @@
-package com.momo.ws.api.keycloak.examplesimpleservice;
-
-
-public record Greeting(long id, String content) { }
---
+++ src/test/java/com/momo/ws/api/keycloak/examplesimpleservice/ExampleSimpleServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.momo.ws.api.keycloak.examplesimpleservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExampleSimpleServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
